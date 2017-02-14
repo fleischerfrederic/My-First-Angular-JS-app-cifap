ae4a42e6600a00da97077802e90a2ae8af4a7b94
fleischerfrederic <fleischerfrederic@gmail.com>
2017-02-14 10:08:59 +0100

    ajout d'angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent } from'./components/mainCmp/app.component';
-import{FormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
 
-import {DashboardComponent} from './components/dashboard/dashboard.component';
-import {EditStudentComponent} from './components/edit.student/edit.student.component';
-import {AddStudentComponent} from './components/add.student/add.student.component';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent,DashboardComponent,EditStudentComponent, AddStudentComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-
- 
- }
+export class AppModule { }
--- src/app/components/dashboard/dashboard.component.ts
+++
@@ -1,7 +0,0 @@
-import{Component} from '@angular/core';
-
-@Component({
-    selector:'dashboard-cmp',
-    templateUrl:'app/components/dashboard/dashboard.component.html'
-})
-export class DashboardComponent{}
